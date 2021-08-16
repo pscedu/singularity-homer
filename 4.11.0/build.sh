c13827e5b22a977b6f3c4f9a5a03472c2a2c579b
icaoberg <icaoberg@andrew.cmu.edu>
2021-08-15 22:39:40 -0400

    Update build.sh

--- 4.11.0/build.sh
+++ 4.11.0/build.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+# Copyright © 2021 Pittsburgh Supercomputing Center.
+# All Rights Reserved.
+
 IMAGE=singularity-homer-4.11.0.sif
 DEFINITION=Singularity
 
+if [ -f $IMAGE ]; then
+	rm -fv $IMAGE
+fi
+
 sudo singularity build $IMAGE $DEFINITION
+
+if [ -f $IMAGE ]; then
+	exit 0
+else
+	exit 1
+fi
