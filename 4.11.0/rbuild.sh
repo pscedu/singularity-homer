edf2c46921417b2eaa5ff9b080fe4dc3a7f8cb61
icaoberg <icaoberg@andrew.cmu.edu>
2021-08-15 22:39:11 -0400

    Update rbuild.sh

--- 4.11.0/rbuild.sh
+++ 4.11.0/rbuild.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+# Copyright © 2021 Pittsburgh Supercomputing Center.
+# All Rights Reserved.
+
 IMAGE=singularity-homer-4.11.0.sif
 DEFINITION=Singularity
 
+if [ -f $IMAGE ]; then
+	rm -fv $IMAGE
+fi
+
 singularity build --remote $IMAGE $DEFINITION
+
+if [ -f $IMAGE ]; then
+	exit 0
+else
+	exit 1
+fi
